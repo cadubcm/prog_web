512e1cce4c1408fb55736951218b9ad56e9e9195
carlos <carlos.mattos@a.ucb.br>
2024-03-14 10:17:38 -0300

    ex

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -1,1 +1,12 @@
+let a = 10;
+let b = 5;
+let c;
 
+c = a + b;
+console.log(`O resultado da operacao entre ${a} e ${b} é ${c}`)
+c = a - b;
+console.log(`O resultado da operacao entre ${a} e ${b} é ${c}`)
+c = a * b
+console.log(`O resultado da operacao entre ${a} e ${b} é ${c}`)
+c = a / b
+console.log(`O resultado da operacao entre ${a} e ${b} é ${c}`)
